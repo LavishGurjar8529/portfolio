9877e68e863220cc1c6c37f30f9c6664ead9e8bd
LavishGurjar8529 <Lavishgurjar8529496261@gmail.com>
2024-07-07 17:12:35 +0530

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,8 +6,7 @@ const HomePage = () => {
   return (
     <div className="container">
       <h1>Welcome to My Personal Website</h1>
-      <p>Hello! I'm John Doe, and this is my personal space on the web where I share a bit about myself and my interests.</p>
-      <button className="button">Learn More About Me</button>
+      <p>Hello! I'm Aakash Gurjar, and this is my personal space on the web where I share a bit about myself and my interests.</p>
     </div>
   );
 };
